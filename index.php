9937b1fa474ed92c3fa0e03a30a7ff1b62880004
Honza Musil <honzamusil@honzamusil.info>
2017-04-19 03:55:01 +0700

    vyhozeny link (zadne css tady neni), oprava <br/>, pridano volani funkce isset() na kontrolu existujiciho $_GET, lepsi zapis volani echo funkce pro vypis jednotlivych radku s vypocty

--- index.php
+++ index.php
@@ -2,35 +2,31 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>ukladani formulare do souboru</title>
-        <link rel="stylesheet" type="text/css" href="styly/styly.css" />
+        <title>Ukladani formulare do souboru</title>
     </head>
     <body>
         <h1>Objektova kalkulacka</h1>
 
         <form method="GET">
-            1.cislo </br>
+            1.cislo <br/>
             <input name="prvniCislo" type="text" size="15" /><br/>
-            2.cislo </br>
+            2.cislo <br/>
             <input name="druheCislo" type="text" size="15" /><br/>
-            <input type="submit" value="vypocitej" /></br>
+            <input type="submit" value="vypocitej" /><br/>
         </form>
 
         <?php
         require_once ('classes/Kalkulacka.php');
-        if ($_GET) {
+        if (isset($_GET)) {
             $kalkulacka = new Kalkulacka();
-            $kalkulacka->prvniCislo = $_GET ['prvniCislo'];
-            $kalkulacka->druheCislo = $_GET ['druheCislo'];
-            echo ("<h2>vysledky</h2>");
-            echo('soucet:');
-            echo($kalkulacka->soucet()) . '</br>';
-            echo('rozdil:');
-            echo($kalkulacka->rozdil()) . '</br>';
-            echo('soucet:');
-            echo($kalkulacka->soucin()) . '</br>';
-            echo('podil:');
-            echo($kalkulacka->podil()) . '</br>';
+            $kalkulacka->prvniCislo = $_GET['prvniCislo'];
+            $kalkulacka->druheCislo = $_GET['druheCislo'];
+            
+            echo "<h2>Vysledky</h2>";
+            echo "soucet: " . $kalkulacka->soucet() . "<br/>";
+            echo "rozdil:" . $kalkulacka->rozdil() . "<br/>";
+            echo "soucet:" . $kalkulacka->soucin() . "<br/>";
+            echo "podil:" . $kalkulacka->podil() . "<br/>";
         }
         ?>
     </body>
