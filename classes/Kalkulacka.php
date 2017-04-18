4f041764bba8fe823f9d6d73c13115f98848ac35
Honza Musil <honzamusil@honzamusil.info>
2017-04-19 03:39:14 +0700

    spravne odsazeni s pouzitim netbeans->source->format

--- classes/Kalkulacka.php
+++ classes/Kalkulacka.php
@@ -1,20 +1,26 @@
 <?php
 
-class Kalkulacka{
+class Kalkulacka {
 
     public $prvniCislo;
     public $druheCislo;
 
-    public function soucet(){
-    return $this->prvniCislo + $this->druheCislo;}
-    
-    public function rozdil(){
-    return $this->prvniCislo - $this->druheCislo;}
-    
-    public function soucin(){   
-    return $this->prvniCislo * $this->druheCislo;} 
+    public function soucet() {
+        return $this->prvniCislo + $this->druheCislo;
+    }
 
-    public function podil(){   
-    return $this->prvniCislo / $this->druheCislo;}
+    public function rozdil() {
+        return $this->prvniCislo - $this->druheCislo;
+    }
+
+    public function soucin() {
+        return $this->prvniCislo * $this->druheCislo;
+    }
+
+    public function podil() {
+        return $this->prvniCislo / $this->druheCislo;
+    }
+
 }
+
 ?>
--- index.php
+++ index.php
@@ -1,38 +1,37 @@
 <!DOCTYPE html>
-    <html>
-        <head>
+<html>
+    <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>ukladani formulare do souboru</title>
-        <link rel="stylesheet" type="text/css" href="styly/styly.css"
-        </head>
-        <body>
+        <link rel="stylesheet" type="text/css" href="styly/styly.css" />
+    </head>
+    <body>
         <h1>Objektova kalkulacka</h1>
 
         <form method="GET">
             1.cislo </br>
-        <input name="prvniCislo" type="text" size="15" /><br/>
+            <input name="prvniCislo" type="text" size="15" /><br/>
             2.cislo </br>
-        <input name="druheCislo" type="text" size="15" /><br/>
-        <input type="submit" value="vypocitej" /></br>
+            <input name="druheCislo" type="text" size="15" /><br/>
+            <input type="submit" value="vypocitej" /></br>
         </form>
-    
-<?php
+
+        <?php
         require_once ('classes/Kalkulacka.php');
-        if ($_GET)
-        {
-           $kalkulacka = new Kalkulacka();
-           $kalkulacka->prvniCislo = $_GET ['prvniCislo'];
-           $kalkulacka->druheCislo = $_GET ['druheCislo'];
-        echo ("<h2>vysledky</h2>");
-        echo('soucet:');
-        echo($kalkulacka->soucet()).'</br>';
-        echo('rozdil:');
-        echo($kalkulacka->rozdil()).'</br>';
-        echo('soucet:');
-        echo($kalkulacka->soucin()).'</br>';
-        echo('podil:');
-        echo($kalkulacka->podil()).'</br>';
+        if ($_GET) {
+            $kalkulacka = new Kalkulacka();
+            $kalkulacka->prvniCislo = $_GET ['prvniCislo'];
+            $kalkulacka->druheCislo = $_GET ['druheCislo'];
+            echo ("<h2>vysledky</h2>");
+            echo('soucet:');
+            echo($kalkulacka->soucet()) . '</br>';
+            echo('rozdil:');
+            echo($kalkulacka->rozdil()) . '</br>';
+            echo('soucet:');
+            echo($kalkulacka->soucin()) . '</br>';
+            echo('podil:');
+            echo($kalkulacka->podil()) . '</br>';
         }
-?>
-        </body>
-    </html>
+        ?>
+    </body>
+</html>
